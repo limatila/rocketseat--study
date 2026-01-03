631f8ef76bcdeeb0af584f123b4bdf0f6d5f569c
Átila Lima <atilalimade@gmail.com>
2026-01-03 16:55:58 -0300

    feat: adicionando comentários descritivos

--- Dispersao/dataframes.py
+++ Dispersao/dataframes.py
@@ -1,8 +1,10 @@
 # %%
+
 from common.utils import DataLoaders
 
 dados = DataLoaders.load_json_to_dict('test-dataframe', parent_path="./Dispersao/data")
 print(dados)
+
 
 # %%
 
@@ -11,25 +13,32 @@ from pandas import DataFrame
 dataframe = DataFrame.from_dict(dados)
 print(dataframe)
 
+
 # %%
+#* Estatísticas descritivas básicas do DataFrame.
 
 dataframe.describe()
+# Exibe contas básicas, desvios, além da medida conforme gráfico de distribuição de amostra normal.
+
 
 # %%
+
 from common.utils import DataPresentation
 
 DataPresentation.print_sequencially(
-    dataframe['altura'].mean(),
-    dataframe['idade'].median(),
-    dataframe['cidade'].mode(),
+    dataframe['altura'].mean(), # Média Aritmética
+    dataframe['idade'].median(), # Mediana
+    dataframe['cidade'].mode(), # Moda
 )
 
 
 # %%
+
 from common.utils import DataPresentation
 
 DataPresentation.print_sequencially(
-    dataframe['idade'].var(),
-    dataframe['altura'].std(),
+    dataframe.idade.var(), # A Variância, medida quantitativa matemática, indica o quão os dados estão espalhados em relação à média.
+    dataframe.idade.std(), # O Desvio Padrão, medida qualitativa real, indica o quão os dados estão dispersos, na unidade de medida original.
+    dataframe.idade.std() / dataframe.idade.mean(), # Coeficiente de Variação, medida qualitativa percentual, indica o quão os dados estão dispersos em relação à média em termos percentuais.
 )
 
