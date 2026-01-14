f09532235158b8b62e22b2e9e517026ab8598d2f
Átila Lima <atilalimade@gmail.com>
2026-01-13 22:08:23 -0300

    refactor: padronizando nome da pasta conforme nome de módulo

--- Medidas/1_posicoes.py
+++ Medidas/1_posicoes.py
@@ -2,7 +2,7 @@
 
 from common.utils import DataLoaders
 
-dados = DataLoaders.load_json_to_dict('test-dataframe', parent_path="./Dispersao/data")
+dados = DataLoaders.load_json_to_dict('test-dataframe', parent_path="./Medidas/data")
 print(dados)
 
 
