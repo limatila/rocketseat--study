dd519e595b730fe45310e78e06990c36f3f62486
Átila Lima <atilalimade@gmail.com>
2026-01-14 23:18:54 -0300

    feat: adicionando mostragens ordenadas, com label específico, e colunas especificadas

--- Graficos/1_representacoes_comuns.py
+++ Graficos/1_representacoes_comuns.py
@@ -23,24 +23,57 @@ dataframe.idade.plot.hist(title='Histograma de Idade', xlabel='Idade', ylabel='F
 
 # %%
 
+import matplotlib.pyplot as plt
+
 #* Barras
 
-bar_kwargs = {
+bars_altura_kwargs = {
     'title': 'Gráfico de Barras de cada Altura',
     'xlabel': 'Participante',
     'ylabel': 'Altura (cm)'
 }
 
-dataframe['altura'].plot.bar( x='nome', y='altura', **bar_kwargs) #especificando o que representa cada eixo
-import matplotlib.pyplot as plt
+#Ordenando decrescentemente o gráfico de barras por altura
+ASC = False
+dataframe_ordenado_altura = dataframe.sort_values(by='altura', ascending=ASC)
 
+dataframe_ordenado_altura['altura'].plot.bar( x='nome', y='altura', **bars_altura_kwargs) #especificando o que representa cada eixo
 
 # Mostra nome de cada participante
 plt.xticks(
-    ticks=range(len(dataframe)),
-    labels=dataframe['nome'],
+    ticks=range(len(dataframe_ordenado_altura)),
+    labels=dataframe_ordenado_altura['nome'],
     rotation=35)
 plt.show()
 
+
+# %%
+
+import matplotlib.pyplot as plt
+
+#* Barras Horizontais
+
+bars_idade_kwargs = {
+    'title': 'Gráfico de Barras Horizontais de Idade',
+    'xlabel': 'Idade',
+    'ylabel': 'Participante'
+}
+
+ASC = True
+dataframe_ordenado_idade = dataframe.sort_values(by='idade', ascending=ASC)
+
+dataframe_ordenado_idade.plot.barh(
+    x='nome',
+    y=['idade', 'altura'], #Mostrando duas colunas no mesmo gráfico
+    **bars_idade_kwargs
+)
+
+
+# Mostrando labels, em y
+plt.yticks(
+    ticks=range(len(dataframe_ordenado_idade)),
+    labels=dataframe_ordenado_idade['nome'],
+    rotation=0)
+plt.show()
 
 # %%
