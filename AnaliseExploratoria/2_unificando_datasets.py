5802e0bbdc209c26f884e7796cfc1aae86fd2e6c
Átila Lima <atilalimade@gmail.com>
2026-01-18 21:15:59 -0300

    feat: unificando dataframes obtidos da source Churns

---
+++ AnaliseExploratoria/2_unificando_datasets.py
@@ -0,0 +1,61 @@
+# %%
+#* Utilizando vários dataframes de CSVs diferentes para realizar a análise exploratória
+#* com o máximo de dados possíveis.
+
+import pandas as pd
+
+from common.utils import DataPresentation
+from common.utils.DataLoaders import load_csv_to_dataframe
+
+PARENT_CHURNS_PATH: str = "./data/datasets/churns/"
+
+df_clientes = load_csv_to_dataframe('churn_customers', parent_path=PARENT_CHURNS_PATH)
+df_contratos = load_csv_to_dataframe('churn_contracts', parent_path=PARENT_CHURNS_PATH)
+df_servicos = load_csv_to_dataframe('churn_services', parent_path=PARENT_CHURNS_PATH)
+
+DataPresentation.print_sequencially(
+    f'Clientes {df_clientes.columns.tolist()}',
+    f'Contratos {df_contratos.columns.tolist()}',
+    f'Serviços {df_servicos.columns.tolist()}',
+    inicial='Dataframes carregados:\n'
+)
+
+DataPresentation.print_sequencially(
+    f'Clientes: {df_clientes.shape}',
+    f'Contratos: {df_contratos.shape}',
+    f'Serviços: {df_servicos.shape}',
+    inicial='Shapes dos Dataframes:\n',
+    early_breakline=True
+)
+
+
+
+
+# %%
+
+#* Unificando datasets
+
+df_unificado = df_clientes.merge(
+    df_contratos,
+    on=['customerID'],
+    how='left'
+)
+
+df_unificado = df_unificado.merge(
+    df_servicos,
+    left_on=['customerID'],
+    right_on=['Customer_ID'],
+    how='left'
+)
+
+# Removendo coluna discrepante
+df_unificado = df_unificado.drop(columns=['Customer_ID'])
+
+df_unificado.info()
+
+print('\nDataframe Unificado - Primeiras 5 linhas:\n')
+df_unificado.head(5)
+        
+
+
+# %%
